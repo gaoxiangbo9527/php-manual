72df29d716771035d67578d019c32f080903a996
gaoxiangbo <gaoxiangbo@meiningec.com>
2018-10-02 17:35:10 +0800

    新增笔记

---
+++ Language Reference/Classes and Objects/Final Keyword/Example #1 Final methods example.php
@@ -0,0 +1,21 @@
+<?php
+class BaseClass
+{
+    public function test()
+    {
+        echo "BaseClass::test() called\n";
+    }
+
+    final public function moreTesting()
+    {
+        echo "BaseClass::moreTesting() called\n";
+    }
+}
+
+class ChildClass extends BaseClass
+{
+    public function moreTesting()
+    {
+        echo "ChildClass::moreTesting() called\n";
+    }
+}
