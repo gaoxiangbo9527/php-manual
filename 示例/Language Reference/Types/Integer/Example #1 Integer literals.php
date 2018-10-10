36951a18284625f0af944842bc47e7809a99f4b7
gaoxiangbo <gaoxiangbo@meiningec.com>
2018-10-10 17:55:52 +0800

    新增笔记

--- 示例/Language Reference/Types/Integer/Example #1 Integer literals.php
+++ 示例/Language Reference/Types/Integer/Example #1 Integer literals.php
@@ -1,9 +1,13 @@
 <?php
+/*
 $a = 1234; // decimal number
 $a = -123; // a negative number
 $a = 0123; // octal number (equivalent to 83 decimal)
 $a = 0x1A; // hexadecimal number (equivalent to 26 decimal)
 $a = 0b11111111; // binary number (equivalent to 255 decimal)
+*/
+//$b = 0xfg;
+//$c = 08;
+$d = 0b2;
 
 echo PHP_INT_SIZE;
-?>
