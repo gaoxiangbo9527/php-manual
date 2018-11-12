cf12284ff34d6ab78bcf64d97d7645067b1755ee
gaoxiangbo <gaoxiangbo@meiningec.com>
2018-11-12 18:20:02 +0800

    新增笔记

---
+++ 示例/Function Reference/Variable and Type Related Extensions/Variable handling/Variable handling Functions/is_callable/Example #2 is_callable() and constructors.php
@@ -0,0 +1,12 @@
+<?php
+
+class Foo
+{
+    public function __construct() {}
+    public function foo() {}
+}
+
+var_dump(
+    is_callable(array('Foo', '__construct')),
+    is_callable(array('Foo', 'foo'))
+);
