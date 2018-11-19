e125ea0918cc0e88c9ea388a5a18a88dd16de7a0
gaoxiangbo <gaoxiangbo@meiningec.com>
2018-11-19 09:09:43 +0800

    新增笔记

---
+++ 示例/Language Reference/Generators/Example #1 Implementing range() as a generator.php
@@ -0,0 +1,43 @@
+<?php
+function xrange($start, $limit, $step = 1) {
+    if ($start < $limit) {
+        if ($step <= 0) {
+            throw new LogicException('Step must be +ve');
+        }
+
+        for ($i = $start; $i <= $limit; $i += $step) {
+            yield $i;
+        }
+    } else {
+        if ($step >= 0) {
+            throw new LogicException('Step must be -ve');
+        }
+
+        for ($i = $start; $i >= $limit; $i -= $step) {
+            yield $i;
+        }
+    }
+}
+
+$result = xrange(1, 20);
+var_dump($result);
+
+/*
+ * Note that both range() and xrange() result in the same
+ * output below.
+ */
+
+echo 'Single digit odd numbers from range():  ';
+foreach (range(1, 9, 2) as $number) {
+    echo "$number ";
+}
+echo "\n";
+
+echo 'Single digit odd numbers from xrange(): ';
+foreach (xrange(1, 9, 2) as $number) {
+    echo "$number ";
+}
+
+foreach ($result as $number) {
+    echo "$number ";
+}
