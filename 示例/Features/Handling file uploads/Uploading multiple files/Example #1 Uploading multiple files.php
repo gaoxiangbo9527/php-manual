6e08b804d8fc977b8abe88d54ef6edadc3ca30fb
gaoxiangbo <815681795@qq.com>
2019-02-24 11:18:47 +0800

    新增内容

---
+++ 示例/Features/Handling file uploads/Uploading multiple files/Example #1 Uploading multiple files.php
@@ -0,0 +1,6 @@
+<form action="file-upload.php" method="post" enctype="multipart/form-data">
+    Send these files:<br />
+    <input name="userfile[]" type="file" /><br />
+    <input name="userfile[]" type="file" /><br />
+    <input type="submit" value="Send files" />
+</form>
