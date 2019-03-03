443ce4658067bea41f54968ac0fea55b3be638aa
gaoxiangbo <815681795@qq.com>
2019-03-03 22:43:07 +0800

    新增内容

---
+++ 示例/Language Reference/Operators/Incrementing And Decrementing Operators/Example #-1.php
@@ -0,0 +1,30 @@
+<?php
+$a = true;
+var_dump(--$a);
+var_dump($a--);
+var_dump(++$a);
+var_dump($a++);
+
+$b = false;
+var_dump(--$b);
+var_dump($b--);
+var_dump(++$b);
+var_dump($b++);
+
+$c = null;
+var_dump(--$c);
+var_dump($c--);
+var_dump(++$c);
+var_dump($c++);
+
+$d = 'd';
+var_dump(--$d);
+var_dump($d--);
+var_dump(++$d);
+var_dump($d++);
+
+$e = 'abcde';
+var_dump(--$e);
+var_dump($e--);
+var_dump(++$e);
+var_dump($e++);
