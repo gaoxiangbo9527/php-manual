d6f15e12ddbb2d47aa3f4f9595667e8b3898e98b
gaoxiangbo <815681795@qq.com>
2020-07-29 10:13:10 +0800

    新增一些与散列相关的字符串函数

---
+++ Examples/Function Reference/Text Processing/Strings/String Functions/md5_file/Example #1 Usage example of md5_file().php
@@ -0,0 +1,5 @@
+<?php
+
+$file = 'php-5.3.0alpha2-Win32-VC9-x64.zip';
+
+echo 'MD5 file hash of ' . $file . ': ' . md5_file($file);
---
+++ Examples/Function Reference/Text Processing/Strings/String Functions/sha1_file/Example #1 sha1_file() example.php
@@ -0,0 +1,9 @@
+<?php
+
+foreach (glob('/home/Kalle/myproject/*.php') as $ent) {
+    if (is_dir($ent)) {
+        continue;
+    }
+
+    echo $ent . ' (SHA1: ' . sha1_file($ent) . ')', PHP_EOL;
+}
