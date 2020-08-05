182db2d6ccbd5118bb19dd70fa3b6cbb7320b8a9
gaoxiangbo <815681795@qq.com>
2020-08-05 20:41:27 +0800

    新增和 运行时配置相关 的一些内容

---
+++ Examples/Function Reference/Affecting PHP's Behaviour/PHP Options and Information/PHP Options And Info Functions/php_ini_scanned_files/Example #1 A simple example to list the returned ini files.php
@@ -0,0 +1,13 @@
+<?php
+
+var_dump(php_ini_scanned_files());
+
+if ($filelist = php_ini_scanned_files()) {
+    if (strlen($filelist) > 0) {
+        $files = explode(',', $filelist);
+
+        foreach ($files as $file) {
+            echo "<li>" . trim($file) . "</li>\n";
+        }
+    }
+}
